9346c231b72ca5c47162a5001e4be7682354f57b
Artem Loshinin <109870624+artem32777@users.noreply.github.com>
2024-01-30 05:58:50 +0400

    Updates

--- assets/favorites.ES56FGwf.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./ProductsList.vue.5LRX8d0n.js";import{u as c}from"./product.store.GJTSXnIA.js";import{f as n,g as a,h as o,o as i}from"./entry.PMburjjp.js";const d=n({__name:"favorites",setup(p){const t=c();return(u,_)=>{const e=s;return i(),a(e,{products:o(t).get&&o(t).get.filter(r=>r.isFavorite),"list-title":"Избранное"},null,8,["products"])}}});export{d as default};
